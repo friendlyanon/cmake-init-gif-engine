ea4d7344a8579860f7b40105497f30477e4f1799
friendlyanon <friendlyanon@users.noreply.github.com>
2021-07-18 22:55:19 +0200

    Implement memory mapping for POSIX

--- test/source/gif_mmap.posix.c
+++ test/source/gif_mmap.posix.c
@@ -1,15 +1,77 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
 #include "gif_mmap.h"
+
+static const char* failed_function_name = NULL;
 
 gif_mmap_span gif_mmap_allocate(const char* path_to_file)
 {
-  (void)path_to_file;
+  void* pointer = NULL;
+  size_t size = 0;
+  void* cleanup_data = NULL;
+  failed_function_name = NULL;
 
-  return (gif_mmap_span) {0};
+  int file_descriptor = open(path_to_file, O_RDONLY);
+  if (file_descriptor == -1) {
+    failed_function_name = "open";
+    goto exit_allocate;
+  }
+
+  struct stat stat_object;
+  if (fstat(file_descriptor, &stat_object) != 0) {
+    failed_function_name = "fstat";
+    goto cleanup_file_descriptor;
+  }
+
+  void* mapping =
+      mmap(NULL, stat_object.st_size, PROT_READ, 0, file_descriptor, 0);
+  if (mapping == MAP_FAILED) {
+    failed_function_name = "mmap";
+    goto cleanup_file_descriptor;
+  }
+
+  pointer = mapping;
+  size = stat_object.st_size;
+  memcpy(&cleanup_data, &file_descriptor, sizeof(int));
+  goto exit_allocate;
+
+cleanup_file_descriptor:
+  close(file_descriptor);
+
+exit_allocate:
+  return (gif_mmap_span) {
+      .pointer = pointer,
+      .size = size,
+      .cleanup_data = cleanup_data,
+  };
 }
 
-void gif_mmap_print_last_error_to_stderr(void) {}
+void gif_mmap_print_last_error_to_stderr(void)
+{
+  if (failed_function_name == NULL) {
+    return;
+  }
+
+  int error_code = errno;
+  const char* error_message = strerror(error_code);
+  fprintf(stderr,
+          "%s failed with error %d: %s",
+          failed_function_name,
+          error_code,
+          error_message);
+}
 
 void gif_mmap_deallocate(gif_mmap_span* span)
 {
-  (void)span;
+  munmap(span->pointer, span->size);
+  int file_descriptor;
+  memcpy(&file_descriptor, &span->cleanup_data, sizeof(int));
+  close(file_descriptor);
 }
