99c5574a887921d1e7540b4952d3a3770ebf6e34
friendlyanon <friendlyanon@users.noreply.github.com>
2021-08-12 03:32:01 +0200

    Move stringifyer generation to build time
    Generating the source file at configure time prevents proper dependency
    tracking, so builds don't happen properly. Adding a custom command to
    generate the file at build time allows dependencies to be setup
    correctly.

--- cmake/stringify-result-code.cmake
+++ cmake/stringify-result-code.cmake
@@ -1,11 +1,13 @@
-# This function extracts the enumerations of the gif_result_code enum and
-# writes a source file to the binary directory that implements stringification
-# of this enum. This is done this way, because Doxygen cannot handle X macros,
-# so it can't be used to implement.
-function(stringify_result_code input output)
-  file(STRINGS "${CMAKE_CURRENT_SOURCE_DIR}/${input}" lines REGEX ",$")
+# This script extracts the enumerations of the gif_result_code enum and writes
+# a source file to the binary directory that implements stringification of this
+# enum. This is done this way, because Doxygen cannot handle X macros, so it
+# can't be used to implement.
 
-  set(source [[
+file(STRINGS "${input}" lines REGEX ",$")
+
+set(source [[
+/* Generated file, do not edit! */
+
 #include "gif_engine/gif_engine.h"
 #include "stddef.h"
 
@@ -13,14 +15,13 @@ const char* gif_result_code_to_string(gif_result_code code)
 {
   switch (code) {
 ]])
-  foreach(line IN LISTS lines)
-    if(NOT line MATCHES "^  ([A-Z0-9_]+),")
-      continue()
-    endif()
-    set(name "${CMAKE_MATCH_1}")
-    string(APPEND source "    case ${name}:\n      return \"${name}\";\n")
-  endforeach()
-  string(APPEND source "  }\n\n  return NULL;\n}\n")
+foreach(line IN LISTS lines)
+  if(NOT line MATCHES "^  ([A-Z0-9_]+),")
+    continue()
+  endif()
+  set(name "${CMAKE_MATCH_1}")
+  string(APPEND source "    case ${name}:\n      return \"${name}\";\n")
+endforeach()
+string(APPEND source "  }\n\n  return NULL;\n}\n")
 
-  file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/${output}" "${source}")
-endfunction()
+file(WRITE "${output}.tmp" "${source}")
